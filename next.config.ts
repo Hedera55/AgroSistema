0b989d8358d77aea4908829521c7fca8e31459c5
Hedera55 <coldcart845@gmail.com>
2026-01-21 13:31:26 -0300

    Fix: Explicitly disable turbopack in config

--- next.config.ts
+++ next.config.ts
@@ -9,6 +9,10 @@ const withPWA = require("next-pwa")({
 
 const nextConfig: NextConfig = {
   /* config options here */
+  // @ts-expect-error - Force webpack for next-pwa support
+  experimental: {
+    turbopack: false
+  }
 };
 
 export default withPWA(nextConfig);
